c5ef1d20ac7852c688a05148beafd2795f2c0064
Hagar Mohammed <73393417+Hajar567@users.noreply.github.com>
2025-11-22 10:40:41 +0200

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,42 +1,80 @@
-#pragma once
 
-template <typename T>
+#ifndef __NODE_H_
+#define __NODE_H_
+
+template < typename T>
 class Node
 {
-private:
-	T value;
-	Node<T>* next;
-	Node<T>* prev;
+private :
+	T item; // A data item
+	Node<T>* next; // Pointer to next node
+	Node<T>* prev; // Pointer to previous node
+public :
+	Node();
+	Node( const T & r_Item);	//passing by const ref.
+	Node( const T & r_Item, Node<T>* nextNodePtr);
+	void setItem( const T & r_Item);
+	void setNext(Node<T>* nextNodePtr);
+	void setPrev(Node<T>* prevNodePtr);
+	T getItem() const ;
+	Node<T>* getNext() const ;
+	Node<T>* getPrev() const;
+}; // end Node
 
-public:
-	Node(T val) {
-		value = val;
-		next = nullptr;
-		prev = nullptr;
-	}
 
-	T getValue() const {
-		return value;
-	}
+template < typename T>
+Node<T>::Node() 
+{
+	next = nullptr;
+} 
 
-	Node<T>* getNext() const {
-		return next;
-	}
+template < typename T>
+Node<T>::Node( const T& r_Item)
+{
+	item = r_Item;
+	next = nullptr;
+} 
 
-	Node<T>* getPrev() const {
-		return prev;
-	}
+template < typename T>
+Node<T>::Node( const T& r_Item, Node<T>* nextNodePtr)
+{
+	item = r_Item;
+	next = nextNodePtr;
+}
+template < typename T>
+void Node<T>::setItem( const T& r_Item)
+{
+	item = r_Item;
+} 
 
-	void setNext(Node<T>* n) {
-		next = n;
-	}
+template < typename T>
+void Node<T>::setNext(Node<T>* nextNodePtr)
+{
+	next = nextNodePtr;
+} 
 
-	void setPrev(Node<T>* p) {
-		prev = p;
-	}
+template < typename T>
+void Node<T>::setPrev(Node<T>* prevNodePtr)
+{
+	prev = prevNodePtr;
+}
 
-	void setValue(T val) {
-		value = val;
-	}
-};
+template < typename T>
+T Node<T>::getItem() const
+{
+	return item;
+} 
 
+template < typename T>
+Node<T>* Node<T>::getNext() const
+{
+	return next;
+} 
+
+template < typename T>
+Node<T>* Node<T>::getPrev() const
+{
+	return prev;
+}
+
+#endif
