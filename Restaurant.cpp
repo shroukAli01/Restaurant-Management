46223697d5260e8f066a7045b4d1c84114371449
shroukAli01 <Shroukali2006@gmail.com>
2025-11-22 10:41:04 +0200

    Add SimpleSimulator method to Restaurant class
    Implemented the SimpleSimulator method for restaurant simulation.

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -1,1 +1,47 @@
+#include "Restaurant.h"
 
+
+
+
+void Restaurant::SimpleSimulator()
+{
+
+    currentTimeStep = 1;
+
+    while (!events.isEmpty() ||
+           !waitingNormal.isEmpty() ||
+           !waitingVegan.isEmpty()  ||
+           !waitingVIP.isEmpty()    ||
+           !inService.isEmpty())
+    {
+        ExecuteEvents();
+      
+        MoveOrdersToInService();
+
+        if (currentTimeStep % 5 == 0)
+            MoveToFinishedEvery5Steps();
+      
+        PrintState();
+        waitForClick();
+
+        currentTimeStep++;
+    }
+}
+
+void Restaurant::ExecuteEvents()
+{
+  
+}
+void Restaurant::MoveOrdersToInService()
+{
+  
+}
+
+void Restaurant::MoveOneOrderToFinished()
+{
+  
+}
+void Restaurant::PrintState()
+{
+  
+}
